22b3c576f367e1f09edf35fefcb66b738c018236
Leon <212649354@qq.com>
2024-02-17 15:21:54 +0800

    窗口居中及关闭及标题制作

--- src/com/itheima/ui/GameJFrame.java
+++ src/com/itheima/ui/GameJFrame.java
@@ -8,7 +8,21 @@ public class GameJFrame extends JFrame {
     //规定：GameJFrame这个界面表示的就是游戏的主界面
     //以后跟游戏相关的所有逻辑都写在这个类中
     public GameJFrame(){
+        //设置界面的宽高
         this.setSize(603, 680);
+
+        //设置界面的标题
+        this.setTitle("拼图单机版 v1.0");
+        //设置界面置顶
+        this.setAlwaysOnTop(true);
+        //设置界面居中
+        this.setLocationRelativeTo(null);
+        //设置关闭模式
+        this.setDefaultCloseOperation(3);
+
+
+
+        //让界面显示出来，建议写在最后
         this.setVisible(true);
     }
 }
--- src/com/itheima/ui/LoginJFrame.java
+++ src/com/itheima/ui/LoginJFrame.java
@@ -9,6 +9,16 @@ public class LoginJFrame extends JFrame {
         //在创建登录界面的时候，同时给这个界面去设置一些信息
         //比如，宽高，直接展示出来
         this.setSize(488, 430);
+
+        //设置界面的标题
+        this.setTitle("拼图 登录");
+        //设置界面置顶
+        this.setAlwaysOnTop(true);
+        //设置界面居中
+        this.setLocationRelativeTo(null);
+        //设置关闭模式
+        this.setDefaultCloseOperation(3);
+
         this.setVisible(true);
     }
 
