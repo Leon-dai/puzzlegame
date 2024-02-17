c79c06e2bd819dc4a9a5fbf285a8f1a5c9ef8bd1
Leon <212649354@qq.com>
2024-02-17 22:13:48 +0800

    打乱图片顺序

--- src/com/itheima/ui/GameJFrame.java
+++ src/com/itheima/ui/GameJFrame.java
@@ -1,21 +1,79 @@
 package com.itheima.ui;
 
 import javax.swing.*;
+import java.util.Random;
 
 public class GameJFrame extends JFrame {
+
+    //4.创建一个二维数组
+    int[][] data = new int[4][4];
+
     //JFrame 界面，窗体
     //子类呢？也表示界面，窗体
     //规定：GameJFrame这个界面表示的就是游戏的主界面
     //以后跟游戏相关的所有逻辑都写在这个类中
-    public GameJFrame(){
+    public GameJFrame() {
         //初始化界面 选中一大段代码抽取成为一个方法快捷键 ctrl alt M
         initJFrame();
 
         //初始化菜单
         initJMenuBar();
 
+        //初始化数据(打乱图片顺序)
+        initData();
+
+
+        //初始化图片(根据打乱之后的结果加载图片)
+        initImage();
+
         //让界面显示出来，建议写在最后
         this.setVisible(true);
+    }
+
+    //初始化数据(打乱)
+    private void initData() {
+        //需求：
+        //把一个一维数组中的数据：0~15 打乱顺序
+        //然后再按照4个一组的方式添加到二维数组当中
+
+        //1.定义一个一维数组
+        int[] tempArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        //2.打乱数组中的数据的顺序
+        //遍历数组，得到每一个元素，拿着每一个元素跟随机索引上的数据进行交换
+        Random r = new Random();
+        for (int i = 0; i < tempArr.length; i++) {
+            //获取到随机索引
+            int index = r.nextInt(tempArr.length);
+            //拿着遍历到的每一个数据，跟随机索引上的数据进行交换
+            int temp = tempArr[i];
+            tempArr[i] = tempArr[index];
+            tempArr[index] = temp;
+        }
+
+        //4.给二维数组添加数据
+        //解法一：
+        //遍历一维数组tempArr得到每一个元素，把每一个元素依次添加到二维数组当中
+        for (int i = 0; i < tempArr.length; i++) {
+            data[i / 4][i % 4] = tempArr[i];
+        }
+    }
+
+    //初始化图片
+    //添加图片的时候，就需要按照二维数组中管理的数据添加图片
+    private void initImage() {
+        //外循环 --- 把内循环重复执行了4次
+        for (int i = 0; i < 4; i++) {
+            //内循环 ---在一行添加了4张图片
+            for (int j = 0; j < 4; j++) {
+                //创建一个JLabel的对象(管理容器)
+                JLabel jLabel = new JLabel(new ImageIcon("image/animal/animal3/" + data[i][j] + ".jpg"));
+                //指定图片位置
+                jLabel.setBounds(105 * j, 105 * i, 105, 105);
+                //把管理容器加到界面中
+                this.getContentPane().add(jLabel);
+            }
+        }
+
     }
 
     private void initJMenuBar() {
@@ -60,5 +118,7 @@ public class GameJFrame extends JFrame {
         this.setLocationRelativeTo(null);
         //设置关闭模式
         this.setDefaultCloseOperation(3);
+        //取消默认的居中放置，只有取消了才会按照XY轴的形式添加组件
+        this.setLayout(null);
     }
 }
